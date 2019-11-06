370f60dca63150e5bddbb900db780761a34b33ba
Olliyze <ollicook@gmail.com>
2019-11-05 23:57:16 -0700

    comment nonantialias.h

--- A7/nonantialias.h
+++ A7/nonantialias.h
@@ -1,10 +1,17 @@
+/*
+* S.I.M.P
+* Team 2
+* https://github.com/University-of-Utah-CS3505/a8-sprite-editor-f19-Mattmathics
+*/
 #ifndef NONANTIALIAS_H
 #define NONANTIALIAS_H
 #include <QWidget>
 #include <QPainter>
 #include <QStyle>
 
-
+/*
+ * Class for disabling anti-aliasing.
+ */
 class nonantialias : public QWidget{
     Q_OBJECT
     Q_DISABLE_COPY(nonantialias)
