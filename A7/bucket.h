7ca3b5bc3c396d3629b8771acc3e6c3c365fe4ed
Olliyze <ollicook@gmail.com>
2019-11-05 23:28:04 -0700

    Comment bucket.h

--- A7/bucket.h
+++ A7/bucket.h
@@ -1,3 +1,8 @@
+/*
+* S.I.M.P
+* Team 2
+* https://github.com/University-of-Utah-CS3505/a8-sprite-editor-f19-Mattmathics
+*/
 #ifndef BUCKET_H
 #define BUCKET_H
 
@@ -5,13 +10,19 @@
 
 #include "tool.h"
 
+/*
+ * Tool for replacing all of the same color that are adjacent to each other and bounded by a different color with
+ * the currently selected brush color.
+ */
 class Bucket : public Tool
 {
 public:
     Bucket(QColor color, Canvas* canvas);
     ~Bucket() override;
-
+    
+    /* Uses a helper recursive method to iterate through the adjacent pixels. */
     void perform(int x, int y) override;
+    
     bool compareColor(QColor *left, QColor *right);
 
 private:
