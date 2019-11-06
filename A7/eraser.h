efb589243580d6732f0680389b6fd7292f035e1a
Olliyze <ollicook@gmail.com>
2019-11-05 23:34:09 -0700

    comment eraser.h

--- A7/eraser.h
+++ A7/eraser.h
@@ -1,8 +1,16 @@
+/*
+* S.I.M.P
+* Team 2
+* https://github.com/University-of-Utah-CS3505/a8-sprite-editor-f19-Mattmathics
+*/
 #ifndef ERASER_H
 #define ERASER_H
 
 #include "tool.h"
 
+/*
+ * A tool to set the selected pixel's rbga value to zero.
+ */
 class Eraser : public Tool
 {
 public:
